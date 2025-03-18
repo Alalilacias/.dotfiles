2df01af01797d23a612d16281351d16bbce5b9b0
Alalilacias <jmor.7913@gmail.com>
2025-03-18 11:53:52 +0100

    Update aliases and Create MD Linter Configuration

---
+++ .config/nvim/lua/plugins/nvim-lint.lua
@@ -0,0 +1,17 @@
+return {
+  {
+    "mfussenegger/nvim-lint",
+    optional = true,
+    opts = {
+      linters = {
+        ["markdownlint-cli2"] = {
+          arts = {
+            "--config",
+            "/home/alalin/.dotfiles/.config/dotfiles/config/.markdownlint-cli2.yaml",
+            "--",
+          },
+        },
+      },
+    },
+  },
+}
